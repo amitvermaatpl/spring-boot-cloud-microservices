6b3cd4bb8afd8ff750a0d56d6ad4566c31c89c65
averma35 <averma35@in.ibm.com>
2019-01-07 17:53:24 +0530

    Adding shoppingcart-ui project

---
+++ spring-cloud-shoppingcart-ui/src/main/java/com/shoppingcart/SpringCloudShoppingcartUiApplication.java
@@ -0,0 +1,14 @@
+package com.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudShoppingcartUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudShoppingcartUiApplication.class, args);
+	}
+
+}
+
