f7c505cc07e9385f18a2dd698ed37c704dca3bc2
averma35 <averma35@in.ibm.com>
2019-01-09 23:09:55 +0530

    Adding Spring Cloud Zuul Proxy as API Gateway in shoppingcart-ui

--- spring-cloud-shoppingcart-ui/src/main/java/com/shoppingcart/SpringCloudShoppingcartUiApplication.java
+++ spring-cloud-shoppingcart-ui/src/main/java/com/shoppingcart/SpringCloudShoppingcartUiApplication.java
@@ -2,12 +2,22 @@ package com.shoppingcart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+import com.shoppingcart.filters.AuthHeaderFilter;
+
+@EnableZuulProxy
 @SpringBootApplication
 public class SpringCloudShoppingcartUiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudShoppingcartUiApplication.class, args);
+	}
+	
+	@Bean
+    AuthHeaderFilter authHeaderFilter() {
+        return new AuthHeaderFilter();
 	}
 
 }
